3a64bfc623c9edecbb28a55b71dae69d962b3abd
DESKTOP-QQQEDJS\kerueilin9 <kerueilin9>
2022-03-31 01:14:15 +0800

    1

--- Final_Project/Final_try/app/src/main/java/com/example/final_try/MyViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.final_try;
-
-
-
-public class MyViewModel extends androidx.lifecycle.ViewModel {
-    public int n = 0;
-}
---
+++ Final_Project/app/src/main/java/com/example/final_project/ViewModel1.java
@@ -0,0 +1,7 @@
+package com.example.final_project;
+
+import androidx.lifecycle.ViewModel;
+
+public class ViewModel1 extends ViewModel {
+    int n = 0;
+}
